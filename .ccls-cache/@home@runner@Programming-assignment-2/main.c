5903dfef9c0ac94b387fb1113333d460d7622a64
Nicholas Newman <33791854+NickNewman5402@users.noreply.github.com>
2024-06-06 02:31:18 +0000

    Fist commit with code
    Added in:
    - struct customers
    - struct node // need to make sure their members are correct
    - createCustomers(customers*) // test cases loop in main
    
    to work on:
    - I need to get the names capitalized. I know about toupper() but this is per char. I need to figure out if there is just a whole string toupper().
    - I need to incorporate these customers in to queues. The placement of the customers is condition dependent.
    
    Implementation Restrictions
    
    1. You must create a struct that stores information about a customer (name, number of smoothies, line number, time entering line). Note that the storage of the line number is redundant, but is designed to ease implementation. DONE
    - Also, you must need to create a function that can create a
    customer using dynamic memory allocation, fill out the customer and then return the customer. You have to use this function whenever you need to create a customer. DONE
    
    2. You must create a node struct for a linked list of customers. This struct should have a pointer to a customer struct, and a pointer to a node struct. DONE
    
    3. You must create a struct to store a queue of customers. This struct should have two pointers – one to the front of the queue and one to the back.
    
    4. You must implement all of the lines as an array of queues of size 12 (stored as a constant). Note that each queue is a linked list.
    
    5. You must dynamically allocate memory as appropriate for linked lists.
    
    6. Your queue must contain the following functions for queue operations and you must use them when applicable :
    a) init
    b) Enqueue
    c) Dequeue
    d) Peek: Return the front of the queue WITHOUT dequeuing
    e) Empty (returns 1 if the queue is empty, 0 if it is not)
    
    7. You should never access the properties of a queue outside of queue related functions. Also, a queue function should not access the properties of another queue. For example, the init function of a queue should access only the properties of that particular queue.
    
    8. You must manage memory appropriately. Specifically, when you dequeue, you should free the memory for the node, but not for the customer. The memory for the customer will be freed later, right after you calculate when the customer will finish checking out.
    
    9. You should not store all the test cases and process them. You need to read each test case at a time and process them.
    
    10. Given the nature of the problem, you can initially place everyone into their respective lines before starting the checkout process. This approach wouldn’t work for all simulations, as some require processing in time order. However, because there is only one counter, this method is feasible here. The key point to remember is when selecting a line. If the current time is 100, for instance, and three lines have customers who arrived before time 100 while the other lines have customers at the front who arrived after time 100, you must ignore the customers in lines where the arrival time is after 100. If all lines have customers who arrived after time 100, you should select the line with the customer who arrived first. Arrival times are unique, so there will be no ties.

--- .ccls-cache/@home@runner@Programming-assignment-2/main.c
+++ .ccls-cache/@home@runner@Programming-assignment-2/main.c
@@ -1,6 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE  12
+
+typedef struct customers{
+
+  char* p_name;
+  int numOfSmootihes;
+  int lineNumber;
+  int enterLineTime;
+  
+}customers;
+
+typedef struct node{
+
+  customers* p_customer;
+  struct node* p_next;
+}node;
+
+node* createNode(node* p_front){
+
+  node* p_newNode = (node*)malloc(sizeof(node));
+  p_newNode->p_next = NULL;
+  
+  return p_newNode;
+}
+
+customers* createCustomers(customers*);
+
+
+customers* createCustomers(customers* p_customer){
+    
+    int m;
+    scanf("%d", &m);
+    p_customer = (customers*)malloc(sizeof(customers)*m);
+      
+      for(int j = 0; j < m; j++){
+        scanf("%d", &p_customer[j].enterLineTime); 
+        scanf("%d", &p_customer[j].lineNumber); 
+        p_customer[j].p_name = (char*)malloc(sizeof(char)*15);
+        scanf("%s", p_customer[j].p_name);
+        /* if(strlen(p_customer[j].p_name) > 15)
+        //   printf("Name is larger than 15 characters");
+
+        // int l = 0;
+        // while (p_customer[j].p_name[l] != '\n') {
+        //     char ch = p_customer[i].p_name[l];
+        //     putchar(toupper(ch));
+        //     j++;
+         }*/
+        scanf("%d", &p_customer[j].numOfSmootihes);
+      }
+  
+  return p_customer;
+}
 
 int main(void) {
-  printf("Hello World\n");
+  
+  customers* customers; 
+  int c; 
+  scanf("%d", &c);
+
+  for(int i =0; i < c; i++){
+  
+    customers = createCustomers(customers);
+
+    // confirming customers are returning to main
+    for(int i = 0; i < 5; i++){
+      printf("\nCustomer [%d] entered line at time %d\n", i, customers[i].enterLineTime);
+      printf("Customer [%d] entered line number %d\n", i, customers[i].lineNumber);
+      printf("Customer [%d] Name is %s\n", i, customers[i].p_name);
+      printf("Customer [%d] ordered %d number of smoothies\n\n", i, customers[i].numOfSmootihes);
+    }// finish test
+    
+  }
+  
   return 0;
 }
